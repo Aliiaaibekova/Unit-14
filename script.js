1d19822a513b73619710f031abead57c8a99c811
AliyaAibekova <88720145+Aliiaaibekova@users.noreply.github.com>
2022-03-22 22:58:37 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const btnToggler = document.querySelector('.btn-toggler');
-const menu = document.querySelector('.menu'); 
-
-
-btnToggler.addEventListener("click", function (){
-    menu.classList.toggle("menu-show");
-});
